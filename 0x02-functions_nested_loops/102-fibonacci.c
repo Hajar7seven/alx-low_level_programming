e63fdefd64a5ccce012b9b52d2aa463330993135
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-03-22 15:31:15 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	i=3;
+
+	long int f = 1, s = 2;
+	long int n = f + s;
+
+	printf("%lu, ", f);
+	printf("%lu, ", s);
+
+	while (i <= 50)
+	{
+		if (i ==50)
+		{
+			printf("%lu \n", n);
+		}
+		else
+		{
+			printf("%lu, ", n);
+		}
+		
+		f = s;
+		s = n;
+		n = f + s;
+		i++;
+	}
+	return (0);
+}
