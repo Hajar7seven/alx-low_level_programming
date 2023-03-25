eb28cfe43bc5a7be27b51bef54aec9f6a3caa6cc
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-03-24 18:52:48 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,14 +3,26 @@
 /**
  * print_diagonal - draws a diagonal line
  *
+ * @n: input variable
+ *
  * Return: always 0
  */
 void print_diagonal(int n)
 {
-	int x;
+	int i;
+	int j;
 
 	if (n > 0)
 	{
-		for (x = 0; x > n; x++)
+		for (i = 0; i > n; i++)
 		{
-			for (
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
