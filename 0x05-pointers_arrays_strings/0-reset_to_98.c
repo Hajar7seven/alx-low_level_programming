7a44ab9a250e071f0d931b4ea1f6c2a68f48040c
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-03-27 07:16:58 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer and updates its value
+ *
+ * @n: value to be replaced
+ * Return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
