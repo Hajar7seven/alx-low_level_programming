cfb2b71d5e07e266c91ac88f01cbb523ef38741b
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-03-30 17:16:13 -0700

    capitalize 2

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @s: string to be capitalized
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
+				|| s[i] == ',' || s[i] == ';'
+				|| s[i] == '.' || s[i] == '!'
+				|| s[i] == '?' || s[i] == '"'
+				|| s[i] == '(' || s[i] == ')'
+				|| s[i] == '{' || s[i] == '}'
+				|| s[i] == '0')
+			s[i] = 32;
+		}
+	}
+	return (s);
+}
