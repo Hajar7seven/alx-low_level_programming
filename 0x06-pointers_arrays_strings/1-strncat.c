24f7efb2e043ae2806eba625c0b595365dcaa66f
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-03-31 17:33:56 -0700

    lll

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,6 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 	for (j= 0; j < n && src[j] != '\0'; j++)
-		dest[i] = src[j];
 	dest[i] = '\0';
 	return (dest);
 }
