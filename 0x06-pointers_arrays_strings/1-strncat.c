54e6cf9df6ef49b8817915e433201c6f755d3fc3
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-04-02 10:43:06 -0700

    copy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,8 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-	for (j= 0; j < n && src[j] != '\0'; j++)
-		dest[i] = src[j];
+	for (j= 0; j < n && src[j] != '\0'; dest[i] = src[j], i++, j++)
 	dest[i] = '\0';
 	return (dest);
 }
