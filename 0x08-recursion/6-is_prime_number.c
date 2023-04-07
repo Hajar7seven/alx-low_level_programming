c15c97778fc7f2b16e61340b92f9aae775126f0b
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-04-06 19:18:47 -0700

    kkk

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _check_if_prime_num - checks if the num is a prime
+ *
+ * @n1: integer
+ * @i: integer
+ *
+ * Return: integer
+ */
+
+int _check_if_prime_num(int n1, int i)
+{
+	if (n1 % i == 0)
+	{
+		return (0);
+	}
+	if (i < 2)
+	{
+		return (0);
+	}
+	if (n1 > i/ 2)
+	{
+		return (1);
+	}
+	return (_check_if_prime_num(n1 + 1, i));
+}
+
+/**
+ * is_prime_number - returns 1 if int is a prime num, otherwise 0
+ *
+ * @n: input int
+ *
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (_check_if_prime_num(2, n));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _check_if_prime_num(int n1, int i);
 
