ad0e42a45d7e43da41fe94e79a63a57071327bda
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-04-10 10:25:28 -0700

    oll

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @src: source string
+ * @dest: destination string
+ * @n: number of strings
+ *
+ * Return: destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
