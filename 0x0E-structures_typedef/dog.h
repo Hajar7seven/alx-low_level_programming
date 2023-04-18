5ca36a5ecdcc2f0d9c1b4bb8fd69899a33522254
Hajar7seven <hajarbenjelloun7@gmail.com>
2023-04-17 17:09:31 -0700

    doggy name

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's information
+ *
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: the dog owner's name
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
